7fa974f03ee375233a3bfb237e009d7e44a06fad
alimanda <alimandasahbana@gmail.com>
2022-12-05 18:15:29 +0700

    first commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,58 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+import axios from 'axios';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
+  public npm: any = '';
+  public nama_mahasiswa: any = '';
+  public jenis_kelamin: any = '';
+  public jurusan: any = '';
+  public foto_siswa: any = '';
 
-  constructor() {}
+  constructor(public toastCtrl: ToastController) {}
 
+  async imageUpload(event) {
+    console.log(event);
+    const file = event.Target.files[0];
+    this.foto_siswa = file;
+  }
+
+  async addData() {
+    const formData = new FormData();
+    formData.append('npm', this.npm);
+    formData.append('nama_mahasiswa', this.nama_mahasiswa);
+    formData.append('jenis_kelamin', this.jenis_kelamin);
+    formData.append('jurusan', this.jurusan);
+    formData.append('foto_siswa', this.foto_siswa);
+    console.log(formData);
+
+    try {
+      const res = await axios.post(
+        'http://localhost/CRUD/post_data.php',
+        formData
+      );
+      console.log(res.data);
+
+      if (res.data.error == false) {
+        const toats = await this.toastCtrl.create({
+          message: 'Data berhasil ditambahkan',
+          duration: 2000,
+        });
+        toats.present();
+      } else {
+        const toats = await this.toastCtrl.create({
+          message: 'Data gagal ditambahkan',
+          duration: 2000,
+        });
+        toats.present();
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import axios from 'axios';
+import { Tab3Page } from '../tab3/tab3.page';
+
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +11,56 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  public mahasiswaData:any = [];
 
+  public npm:any = "";
+  public nama_mahasiswa:any = "";
+  public jenis_kelamin:any = "";
+  public jurusan:any = "";
+  public foto_siswa:any = "";
+
+  constructor(
+    public modalctrl: ModalController,
+  ) {
+    this.getData(); 
+  }
+
+
+  async getData() {
+    try{
+      const res = await axios.get('http://localhost/CRUD/get_data_mahasiswa.php');
+      this.mahasiswaData = res.data.result;
+      console.log(this.mahasiswaData);
+    }catch(err){
+      console.log(err);
+    }
+  }
+
+async deleteData(id) {
+  const fd = new FormData();
+  fd.append('id',id);
+  try{
+    const res = await axios.post('http://localhost/CRUD/delete_data_mahasiswa.php', fd);
+    if(res.data.success == false){
+    alert('Gagal menghapus data');
+  }else{
+    alert('Berhasil menghapus data');
+    this.getData();
+  }
+
+  }catch(err){
+    console.log(err);
+  }
+}
+
+async getDataMahasiswa(id) {
+  console.log(id);
+  const modal = await this.modalctrl.create({
+    component: Tab3Page,
+    componentProps: {
+      "id": id
+    }
+  });
+  return await modal.present();
+}
 }
